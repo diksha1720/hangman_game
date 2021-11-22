7a548008d37daadd20eeb4a25c54bdeee5ed2939
diksha choudhary <43616845+diksha1720@users.noreply.github.com>
2021-11-22 13:37:03 +0000

    improved code readability

--- main.py
+++ main.py
@@ -1,88 +1,49 @@
 
 import random
+import ascii_art
+import words
+
+def letter_match(display,j):
+  flag=1
+  display[j]=chosen_word[j]
+  print(f"{' '.join(display)}")
+  return flag
+
+def letter_not_match(guess,chances,stages):
+  print(f"You chose letter '{guess}' which is not in the word! You lose a life!!")
+  chances-=1
+  print(stages[chances])
+  return chances
+
+def print_result(chances,chosen_word):
+  if chances==0:
+    print("Game Over!!! You lose")
+  else:
+    print(f"You Win!!! You guessed it right the word is {chosen_word}")
+
+def correct_guess(chosen_word,guess,j):
+  if chosen_word[j] == guess and display[j]=="_":
+    return True
+  return False
+
+print(ascii_art.logo)
+chosen_word = random.choice(words.word_list)
+print(f"Choosen word is : {chosen_word}")
+stages=ascii_art.stages
+chances=6
+display=['_']*len(chosen_word)
 
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["aardvark", "baboon", "camel"]
-
-chosen_word = random.choice(word_list)
-
-display=["_"]*len(chosen_word)
-
-chances= 6
 while(chances!=0):
   guess = input("Guess a letter: ").lower()
   for j in range(len(chosen_word)):
     flag=-1
-    if chosen_word[j] == guess and display[j]=="_":
-      flag=1
-      display[j]=chosen_word[j]
-      print(display)
+    if correct_guess(chosen_word,guess,j):
+      flag=letter_match(display,j)
       break
   if flag==-1:
-      chances-=1
-      print(stages[chances])
+    chances=letter_not_match(guess,chances,stages)
+    
   if '_' not in display:
     break
-if chances==0:
-  print("Game Over!!! You lose")
-else:
-  print(f"You Win!!! You guessed it right the word is {chosen_word}")
-
-
-
+print_result(chances,chosen_word)
